30961c2d41b53135437adf65c82163d22af0c26f
Brando-lag <84989235+Brando-lag@users.noreply.github.com>
2021-05-31 12:35:35 -0500

    Add files via upload

---
+++ jsfisica.js
@@ -0,0 +1,13 @@
+function LimpiarFormulario(){
+	document.getElementById("miForm").reset();
+}
+
+var velocidad = function(n1,n2){
+
+	var dis = parseFloat(document.getElementById("dis").value);
+	var tie = parseFloat(document.getElementById("tie").value);
+	var resultado  = dis/tie;
+
+
+	return resultado;
+}
